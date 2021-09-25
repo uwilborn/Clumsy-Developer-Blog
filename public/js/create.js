37df422385199ba32ce0c001c10b99d6c281b1ab
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-25 12:23:47 -0400

    Create create.js

---
+++ public/js/create.js
@@ -0,0 +1,26 @@
+var  createBtn = document.getElementById("create")
+
+async function createPost(){
+  var note = {
+    name : document.getElementById("name").value,
+    title: document.getElementById("posttitle").value,
+    created_by: document.getElementById("createdby").value
+  }
+  console.log(note)
+ 
+    const response = await fetch('/api/projects/create', {
+      method: 'POST',
+      body: JSON.stringify(note),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+
+}
+
+createBtn.addEventListener("click",createPost)
